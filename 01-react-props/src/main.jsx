58537704ca73fa2ad3ece40ad9ecda1939f6152c
Luis <jesuslara.luis99@gmail.com>
2025-07-08 21:00:54 -0600

    Agregando template basico web

--- 01-react-props/src/main.jsx
+++ 01-react-props/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App saludo= "Hola que tal" title = "G41A esto es un prop" />
-    <App saludo= "Hi" title = "this another component with diferent info" />
+    <App/>
   </StrictMode>,
 )
