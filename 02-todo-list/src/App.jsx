30c9e6f81cf8a9e560e6d9bbeccf9699b45e2058
Luis <jesuslara.luis99@gmail.com>
2025-07-10 20:36:30 -0600

    add-ToDoItem

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -1,10 +1,27 @@
+/* 1. Importar el hook useState */
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-
+function App () {
+  /* Creo el estado donde guardare la informacion del input */
+  const [inputValue, setInputValue] = useState('')
+  /* 3.Crear una funcion que se ejecutara cuando haga clic en anadir */
+  const handleAdd = () => { console.log('Anadir tarea:', inputValue) }
   return (
     <>
-    
+      <h1>Todo List</h1>
+      <input
+        type='text'
+        placeholder='Escribe una tarea'
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={handleAdd}>Añadir</button>
+      <ul>
+        <li>item 1<button>Eliminar</button></li>
+        <li>item 2<button>Eliminar</button></li>
+        <li>item 3<button>Eliminar</button></li>
+      </ul>
     </>
   )
 }
