e4b7507ae9d7e2066f01f2891ae3276dd2e173b4
Luis <jesuslara.luis99@gmail.com>
2025-07-15 19:19:12 -0600

    update files_ and_clean_3_proyect

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App () {
-  /* Creo el estado donde guardare la informacion del input */
+  /* 2. Creo el estado donde guardare la informacion del input */
   const [inputValue, setInputValue] = useState('')
   /* 3.Crear una funcion que se ejecutara cuando haga clic en anadir */
   const handleAdd = () => { console.log('Anadir tarea:', inputValue) }
   return (
     <>
       <h1>Todo List</h1>
+      {/* 4. Implementar el evento onChange para guardar la información del estado, apenas esta sea tecleada */}
       <input
         type='text'
         placeholder='Escribe una tarea'
---
+++ Entregables/intro-a-react/src/App.jsx
@@ -0,0 +1,14 @@
+function App(props) {
+
+  
+  <Usuario nombre="Ana" />
+  return (
+    <>
+     <h1>Hola mundo</h1>
+     <h1>Hola, {props.nombre}!</h1>;
+     
+    </>
+  )
+}
+
+export default App
