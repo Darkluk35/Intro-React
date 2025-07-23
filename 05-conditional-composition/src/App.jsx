8e0112527b5dba36c482f226e498a69e207f98e8
Luis <jesuslara.luis99@gmail.com>
2025-07-22 19:24:11 -0600

    Coditional composition proyect

---
+++ 05-conditional-composition/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <div className='contaioner'>
+        <h1>Mi panel de control</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
