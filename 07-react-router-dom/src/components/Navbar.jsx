18dd8314aa472f36fb9368d4546ee4135b3eb543
Luis <jesuslara.luis99@gmail.com>
2025-07-29 19:54:17 -0600

    Navegacion de las paginas

--- 07-react-router-dom/src/components/Navbar.jsx
+++ 07-react-router-dom/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/characters'>Personajes</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 export default Navbar
---
+++ 07-react-router-dom/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import Characters from '../pages/Characters'
+import CharacterDetail from '../pages/CharacterDetails'
+import NotFound from '../pages/NotFound'
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/characters' element={<Characters />} />
+      <Route path='/characters/:id' element={<CharacterDetail />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  )
+}
+export default AppRoutes
