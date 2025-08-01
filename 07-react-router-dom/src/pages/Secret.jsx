eb9409038f37e74aa372d91c8a438d11254053d5
Luis <jesuslara.luis99@gmail.com>
2025-07-31 19:05:11 -0600

    agregar pagina secreta de login user

---
+++ 07-react-router-dom/src/pages/Secret.jsx
@@ -0,0 +1,11 @@
+const Secret = () => {
+  return (
+    <div className='container'>
+      <h1>Seccion secreta</h1>
+      <p>Solo un usuario autenticado puede ver esta ágina.</p>
+      <p>Ahora estas autenticado!</p>
+    </div>
+  )
+}
+
+export default Secret
